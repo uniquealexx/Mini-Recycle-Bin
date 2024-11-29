eaef88a65c627aa0e057adbaac4a0ff20fa1a42d
uniquealexx <cutecubed1337@gmail.com>
2024-11-29 11:02:19 +0300

    update # Add simple config system # Add properties window # Add config for save switched theme # Something fixes for code

--- MiniBin/main.cpp
+++ MiniBin/main.cpp
@@ -2,12 +2,74 @@
 #include <shellapi.h>
 #include <shlobj.h>
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <map>
 
 enum class EActions
 {
     Clear = 1,
     ChangeTheme,
-    Exit
+    Exit,
+    Properties
+};
+
+class Config
+{
+public:
+    Config(const std::string& filename) : strFileName(filename)
+    {
+        Load();
+    }
+
+    void Set(const std::string& key, const std::string& value)
+    {
+        mapConfig[key] = value;
+    }
+
+    std::string Get(const std::string& key) const
+    {
+        auto it = mapConfig.find(key);
+        if (it != mapConfig.end())
+            return it->second;
+
+        return "";
+    }
+
+    void Save() const
+    {
+        std::ofstream outfile(strFileName);
+        if (outfile.is_open())
+        {
+            for (const auto& pair : mapConfig)
+                outfile << pair.first << "=" << pair.second << std::endl;
+
+            outfile.close();
+        }
+    }
+
+    void Load() {
+        std::ifstream infile(strFileName);
+        if (infile.is_open())
+        {
+            std::string line;
+            while (std::getline(infile, line))
+            {
+                size_t pos = line.find('=');
+                if (pos != std::string::npos)
+                {
+                    std::string key = line.substr(0, pos);
+                    std::string value = line.substr(pos + 1);
+                    mapConfig[key] = value;
+                }
+            }
+            infile.close();
+        }
+    }
+
+private:
+    std::string strFileName;
+    std::map<std::string, std::string> mapConfig;
 };
 
 #define WM_CONTEXT_NOTIFY (WM_USER + 1)
@@ -20,6 +82,8 @@ HICON hIconFull;
 HICON hIconRecycler;
 HICON hIconRecyclerFull;
 BOOL bUseDefaultIcons = TRUE;
+HWND hPropertiesWnd = NULL;
+Config config("config.cfg");
 
 BOOL IsRecycleBinEmpty()
 {
@@ -40,25 +104,60 @@ void UpdateTrayIcon()
     Shell_NotifyIcon(NIM_MODIFY, &NotifyData);
 }
 
+LRESULT CALLBACK PropertiesWindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg)
+    {
+    case WM_CLOSE:
+        DestroyWindow(hWnd);
+        hPropertiesWnd = NULL;
+        break;
+    default:
+        return DefWindowProc(hWnd, uMsg, wParam, lParam);
+    }
+    return 0;
+}
+
 LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
     switch (uMsg)
     {
     case WM_COMMAND:
-        if (LOWORD(wParam) == (int)EActions::Clear)
+        switch (LOWORD(wParam))
         {
+        case (int)EActions::Clear:
             SHEmptyRecycleBin(NULL, NULL, SHERB_NOCONFIRMATION | SHERB_NOPROGRESSUI | SHERB_NOSOUND);
             UpdateTrayIcon();
-        }
-        else if (LOWORD(wParam) == (int)EActions::ChangeTheme)
-        {
+            break;
+        case (int)EActions::ChangeTheme:
             bUseDefaultIcons = !bUseDefaultIcons;
+            config.Set("theme", bUseDefaultIcons ? "1" : "0");
+            config.Save();
             UpdateTrayIcon();
-        }
-        else if (LOWORD(wParam) == (int)EActions::Exit)
-        {
+            break;
+        case (int)EActions::Exit:
             Shell_NotifyIcon(NIM_DELETE, &NotifyData);
             PostQuitMessage(0);
+            break;
+        case (int)EActions::Properties:
+            if (hPropertiesWnd == NULL)
+            {
+                WNDCLASS wc = { 0 };
+                wc.lpfnWndProc = PropertiesWindowProc;
+                wc.hInstance = hInst;
+                wc.lpszClassName = L"PropertiesWindowClass";
+                RegisterClass(&wc);
+
+                hPropertiesWnd = CreateWindow(L"PropertiesWindowClass", L"Properties", WS_OVERLAPPEDWINDOW,
+                    CW_USEDEFAULT, CW_USEDEFAULT, 600, 800, NULL, NULL, hInst, NULL);
+
+                ShowWindow(hPropertiesWnd, SW_SHOW);
+                UpdateWindow(hPropertiesWnd);
+                break;
+            }
+
+            SetForegroundWindow(hPropertiesWnd);
+            break;
         }
         break;
     case WM_DESTROY:
@@ -73,6 +172,7 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
             HMENU hMenu = CreatePopupMenu();
             AppendMenu(hMenu, MF_STRING, (int)EActions::Clear, L"Clear");
             AppendMenu(hMenu, MF_STRING, (int)EActions::ChangeTheme, L"Change theme");
+            AppendMenu(hMenu, MF_STRING, (int)EActions::Properties, L"Properties");
             AppendMenu(hMenu, MF_STRING, (int)EActions::Exit, L"Exit");
             SetForegroundWindow(hWnd);
             POINT Point;
@@ -94,6 +194,8 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
     hInst = hInstance;
+    
+    bUseDefaultIcons = config.Get("theme") == "0" ? FALSE : TRUE;
 
     WNDCLASS WndClass = { 0 };
     WndClass.lpfnWndProc = WindowProc;
