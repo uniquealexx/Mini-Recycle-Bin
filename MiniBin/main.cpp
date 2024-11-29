80a2fed357377f4078296e4362f4565f445bf077
Hatoribocchi <sublime1337@mail.ru>
2024-11-29 10:22:55 +0300

    $$$

--- MiniBin/main.cpp
+++ MiniBin/main.cpp
@@ -1,158 +1,156 @@
 ﻿#include <windows.h>
 #include <shellapi.h>
 #include <shlobj.h>
+#include <iostream>
 
-#define ID_TRAY_ICON 1
-#define ID_EXIT_MENU 2
-#define ID_CLEAR_MENU 3
-#define ID_CHANGE_THEME_MENU 4
-#define ID_TIMER 5
-#define TIMER_INTERVAL 1000
+enum class EActions
+{
+    Clear = 1,
+    ChangeTheme,
+    Exit
+};
+
+#define WM_CONTEXT_NOTIFY (WM_USER + 1)
+#define WM_ICON_CHANGE_NOTIFY (WM_USER + 2)
 
 HINSTANCE hInst;
-NOTIFYICONDATA nid;
+NOTIFYICONDATA NotifyData;
 HICON hIconEmpty;
 HICON hIconFull;
 HICON hIconRecycler;
 HICON hIconRecyclerFull;
-BOOL useDefaultIcons = TRUE;
+BOOL bUseDefaultIcons = TRUE;
 
-BOOL IsRecycleBinEmpty() 
+BOOL IsRecycleBinEmpty()
 {
-    SHQUERYRBINFO rbi = { sizeof(SHQUERYRBINFO) };
-    if (SHQueryRecycleBin(NULL, &rbi) == S_OK) 
-        return (rbi.i64NumItems == 0);
+    SHQUERYRBINFO RbiInfo = { sizeof(SHQUERYRBINFO) };
+    if (SHQueryRecycleBin(NULL, &RbiInfo) == S_OK)
+        return (RbiInfo.i64NumItems == 0);
 
     return TRUE;
 }
 
-void UpdateTrayIcon() 
+void UpdateTrayIcon()
 {
-    if (useDefaultIcons) 
-    {
-        if (IsRecycleBinEmpty())
-            nid.hIcon = hIconRecycler;
-        else
-            nid.hIcon = hIconRecyclerFull;
-    }
-    else 
-    {
-        if (IsRecycleBinEmpty())
-            nid.hIcon = hIconEmpty;
-        else
-            nid.hIcon = hIconFull; 
-    }
+    if (IsRecycleBinEmpty())
+        NotifyData.hIcon = bUseDefaultIcons ? hIconRecycler : hIconEmpty;
+    else
+        NotifyData.hIcon = bUseDefaultIcons ? hIconRecyclerFull : hIconFull;
 
-    Shell_NotifyIcon(NIM_MODIFY, &nid);
+    Shell_NotifyIcon(NIM_MODIFY, &NotifyData);
 }
 
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-    switch (uMsg) 
+    switch (uMsg)
     {
     case WM_COMMAND:
-        if (LOWORD(wParam) == ID_EXIT_MENU) 
-        {
-            KillTimer(hwnd, ID_TIMER);
-            Shell_NotifyIcon(NIM_DELETE, &nid);
-            PostQuitMessage(0);
-        }
-        else if (LOWORD(wParam) == ID_CLEAR_MENU) 
+        if (LOWORD(wParam) == (int)EActions::Clear)
         {
             SHEmptyRecycleBin(NULL, NULL, SHERB_NOCONFIRMATION | SHERB_NOPROGRESSUI | SHERB_NOSOUND);
             UpdateTrayIcon();
         }
-        else if (LOWORD(wParam) == ID_CHANGE_THEME_MENU) 
+        else if (LOWORD(wParam) == (int)EActions::ChangeTheme)
         {
-            useDefaultIcons = !useDefaultIcons;
+            bUseDefaultIcons = !bUseDefaultIcons;
             UpdateTrayIcon();
+        }
+        else if (LOWORD(wParam) == (int)EActions::Exit)
+        {
+            Shell_NotifyIcon(NIM_DELETE, &NotifyData);
+            PostQuitMessage(0);
         }
         break;
     case WM_DESTROY:
-        KillTimer(hwnd, ID_TIMER);
-        Shell_NotifyIcon(NIM_DELETE, &nid);
+        Shell_NotifyIcon(NIM_DELETE, &NotifyData);
         PostQuitMessage(0);
         break;
-    case WM_USER + 1:
-        if (lParam == WM_LBUTTONDBLCLK) 
-        {
+    case WM_CONTEXT_NOTIFY:
+        if (lParam == WM_LBUTTONDBLCLK)
             ShellExecute(NULL, L"open", L"shell:RecycleBinFolder", NULL, NULL, SW_SHOWNORMAL);
-        }
-        else if (lParam == WM_RBUTTONUP) 
+        else if (lParam == WM_RBUTTONUP)
         {
             HMENU hMenu = CreatePopupMenu();
-            AppendMenu(hMenu, MF_STRING, ID_CLEAR_MENU, L"Clear");
-            AppendMenu(hMenu, MF_STRING, ID_CHANGE_THEME_MENU, L"Change theme");
-            AppendMenu(hMenu, MF_STRING, ID_EXIT_MENU, L"Exit");
-            SetForegroundWindow(hwnd);
-            POINT pt;
-            GetCursorPos(&pt);
-            TrackPopupMenu(hMenu, TPM_LEFTALIGN, pt.x, pt.y, 0, hwnd, NULL);
-            PostMessage(hwnd, WM_NULL, 0, 0);
+            AppendMenu(hMenu, MF_STRING, (int)EActions::Clear, L"Clear");
+            AppendMenu(hMenu, MF_STRING, (int)EActions::ChangeTheme, L"Change theme");
+            AppendMenu(hMenu, MF_STRING, (int)EActions::Exit, L"Exit");
+            SetForegroundWindow(hWnd);
+            POINT Point;
+            GetCursorPos(&Point);
+            TrackPopupMenu(hMenu, TPM_LEFTALIGN, Point.x, Point.y, 0, hWnd, NULL);
+            PostMessage(hWnd, WM_NULL, 0, 0);
             DestroyMenu(hMenu);
         }
         break;
-    case WM_TIMER:
-        if (wParam == ID_TIMER) 
-        {
-            UpdateTrayIcon();
-        }
+    case WM_ICON_CHANGE_NOTIFY:
+        UpdateTrayIcon();
         break;
     default:
-        return DefWindowProc(hwnd, uMsg, wParam, lParam);
+        return DefWindowProc(hWnd, uMsg, wParam, lParam);
     }
     return 0;
 }
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) 
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
     hInst = hInstance;
 
-    WNDCLASS wc = { 0 };
-    wc.lpfnWndProc = WindowProc;
-    wc.hInstance = hInstance;
-    wc.lpszClassName = L"MiniBin";
-    RegisterClass(&wc);
+    WNDCLASS WndClass = { 0 };
+    WndClass.lpfnWndProc = WindowProc;
+    WndClass.hInstance = hInstance;
+    WndClass.lpszClassName = L"MiniBin";
+    RegisterClass(&WndClass);
 
-    HWND hwnd = CreateWindow(wc.lpszClassName, L"Mini Bin", 0, 0, 0, 0, 0, HWND_MESSAGE, NULL, hInstance, NULL);
+    HWND hWnd = CreateWindow(WndClass.lpszClassName, L"Mini Bin", 0, 0, 0, 0, 0, HWND_MESSAGE, NULL, hInstance, NULL);
 
-    nid.cbSize = sizeof(NOTIFYICONDATA);
-    nid.hWnd = hwnd;
-    nid.uID = ID_TRAY_ICON;
-    nid.uFlags = NIF_ICON | NIF_MESSAGE | NIF_TIP;
-    nid.uCallbackMessage = WM_USER + 1;
+    NotifyData.cbSize = sizeof(NOTIFYICONDATA);
+    NotifyData.hWnd = hWnd;
+    NotifyData.uID = NULL;
+    NotifyData.uFlags = NIF_ICON | NIF_MESSAGE | NIF_TIP;
+    NotifyData.uCallbackMessage = WM_CONTEXT_NOTIFY;
 
-    SHSTOCKICONINFO sii;
-    sii.cbSize = sizeof(SHSTOCKICONINFO);
+    SHSTOCKICONINFO shIconInfo;
+    shIconInfo.cbSize = sizeof(SHSTOCKICONINFO);
 
     hIconEmpty = (HICON)LoadImage(NULL, L"Icons/EmptyTrash.ico", IMAGE_ICON, 0, 0, LR_LOADFROMFILE | LR_SHARED);
     hIconFull = (HICON)LoadImage(NULL, L"Icons/FullTrash.ico", IMAGE_ICON, 0, 0, LR_LOADFROMFILE | LR_SHARED);
 
-    if (SUCCEEDED(SHGetStockIconInfo(SIID_RECYCLER, SHGSI_ICON | SHGSI_LARGEICON, &sii))) 
-        hIconRecycler = sii.hIcon;
-    if (SUCCEEDED(SHGetStockIconInfo(SIID_RECYCLERFULL, SHGSI_ICON | SHGSI_LARGEICON, &sii))) 
-        hIconRecyclerFull = sii.hIcon;
+    if (SUCCEEDED(SHGetStockIconInfo(SIID_RECYCLER, SHGSI_ICON | SHGSI_LARGEICON, &shIconInfo)))
+        hIconRecycler = shIconInfo.hIcon;
+    if (SUCCEEDED(SHGetStockIconInfo(SIID_RECYCLERFULL, SHGSI_ICON | SHGSI_LARGEICON, &shIconInfo)))
+        hIconRecyclerFull = shIconInfo.hIcon;
 
-    if (!hIconEmpty || !hIconFull || !hIconRecycler || !hIconRecyclerFull) 
+    if (!hIconEmpty || !hIconFull || !hIconRecycler || !hIconRecyclerFull)
     {
         MessageBox(NULL, L"Failed to load icon!", L"Error", MB_ICONERROR);
         return 1;
     }
 
-    lstrcpy(nid.szTip, L"Mini Bin Tools");
+    lstrcpy(NotifyData.szTip, L"Mini Bin Tools");
     UpdateTrayIcon();
-    Shell_NotifyIcon(NIM_ADD, &nid);
+    Shell_NotifyIcon(NIM_ADD, &NotifyData);
 
-    SetTimer(hwnd, ID_TIMER, TIMER_INTERVAL, NULL);
+    SHChangeNotifyEntry shCNE;
+    shCNE.pidl = NULL;
+    shCNE.fRecursive = FALSE;
 
-    MSG msg;
-    while (GetMessage(&msg, NULL, 0, 0)) 
+    ULONG hNotify = SHChangeNotifyRegister(hWnd, SHCNRF_ShellLevel, SHCNE_UPDATEITEM | SHCNE_UPDATEIMAGE | SHCNE_RMDIR | SHCNE_MKDIR | SHCNE_DELETE, WM_ICON_CHANGE_NOTIFY, 1, &shCNE);
+    if (!hNotify)
     {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
+        MessageBox(NULL, L"Failed to register for notifications!", L"Error", MB_ICONERROR);
+        return 1;
     }
 
-    KillTimer(hwnd, ID_TIMER);
-    Shell_NotifyIcon(NIM_DELETE, &nid);
-    return (int)msg.wParam;
+    MSG Message;
+    while (GetMessage(&Message, NULL, 0, 0))
+    {
+        TranslateMessage(&Message);
+        DispatchMessage(&Message);
+    }
+
+    if (hNotify)
+        SHChangeNotifyDeregister(hNotify);
+
+    Shell_NotifyIcon(NIM_DELETE, &NotifyData);
+    return (int)Message.wParam;
 }
